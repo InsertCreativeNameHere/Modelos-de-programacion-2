f2d7c2e957ae77a131c9fc5a5333d825aeeca040
Javier Aponte <48999640+InsertCreativeNameHere@users.noreply.github.com>
2019-10-04 15:46:50 -0500

    ver 1.0

---
+++ Funciones.hs
@@ -0,0 +1,57 @@
+cuadrado::Int->Int
+cuadrado x = x * x
+
+infinito :: Integer
+infinito = 1 + infinito
+
+tres :: Integer->Integer
+tres x = 3
+
+sumar::[Int]->Int
+sumar [ ] = 0
+sumar (x:xs) = x + sumar(xs)
+
+invertir::Ord a=>[a]->[a]
+invertir [ ] = [ ]
+invertir (x:xs) = (invertir xs)++[x]
+
+igualLista:: Eq a => [a]->[a]->Bool
+igualLista l1 l2 = l1 == l2
+
+lista_ordenada::Ord a=>[a]->Bool
+lista_ordenada [] = True
+lista_ordenada [_] = True
+lista_ordenada (x:y:xs) = (x<=y) && lista_ordenada(y:xs)
+
+mostrar_ubicacion::Ord a=>[a]->Int->a
+mostrar_ubicacion l n = l!!n
+
+mayor::[Int]->Int
+mayor [] = 0
+mayor (x:xs)| x > mayor (xs) = x
+            | otherwise = mayor (xs)
+
+factorial::Int->Int
+factorial 0 = 1
+factorial n = n * factorial(n-1)
+
+contarpares::[Int]->Int
+contarpares []=0
+contarpares lista= length [x | x <- lista, mod x 2 ==0]
+
+divisible::Int->Int->Bool
+divisible x y = (mod x y) ==0
+
+divisibles::Int->[Int]
+divisibles x = [y | y <-[1..x],divisible x y]
+
+esPrimo::Int->Bool
+esPrimo n = length (divisibles n) <=2
+
+primos::Int->[Int]
+primos n = [x | x <-[1..n],esPrimo x]
+
+cuadrados::[Int]->[Int]
+cuadrados [ ] = [ ]
+cuadrados l = [x*x| x <- l]
+
--- Funciones.py
+++ Funciones.py
@@ -1,33 +1,76 @@
 # -*- coding: utf-8 -*-
 """
 Editor de Spyder
-
 Codigo realizado por
 Luis Daniel Hormiga González 20172020026
 Joan Sebastian Perez Mejía 20161020055
 Javier Andres Aponte Quevedo 20172020036
 """
 
-def factorial(n):
-    if n == 0:
-        return 1
-    return n*factorial(n-1)
 
-def cuadrado(n):
-    return n*n
+def cuadrado(x):
+    return x*x
 
-def infinito(n):
-    return 1+infinito(n)
+def infinito():
+    return 1 + infinito
 
-def tres(n):
+def tres(x):
     return 3
 
-def sumarArray(n):
-    if n == []:
+def sumar(x):
+    if x == []:
         return 0
-    return n[0] + sumarArray(n[1:])
+    return x[0]  + sumar(x[1:])
 
+def invertir(x):
+    if x == []:
+        return []
+    return invertir(x[1:]) + [x[0]]
 
-        
+def igualLista(x,y):
+    return x==y
+
+def lista_ordenada(x):
+    if x == []:
+        return True
+    if x == [" "]:
+        return True
+    return (x[0] <= x[1]) and lista_ordenada(x[1:])
+
+def mostrarUbicacion(x,u):
+    return x[u]
+
+def mayor(x):
+    if x == []:
+        return 0
+    if x[0] > mayor(x[1:]):
+        return x[0]
+    else:
+        return mayor(x[1:])
     
+def factorial(x):
+    if x == 0:
+        return 1
+    return x * factorial(x-1)
+
+def contarPares(x):
+    if x==[]:
+      return 0
+    return  len(list(y for y in x if y%2 == 0))
+
+def divisible(x,y):
+    if x%y == 0:
+        return True
+    return False
+def divisibles(x):
+    return list(y for y in range(1,x+1) if divisible(x,y))
+
+def esPrimo(x):
+    return len(divisibles(x)) <= 2
+
+def primos(x):
+    return list(y for y in range(1,x+1) if esPrimo(y))
+
+def cuadrados(x):
+    return list(y*y for y in x)
 
